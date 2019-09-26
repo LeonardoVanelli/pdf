ab134224087e78c4640426bd7ebbfe544cc33a47
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2019-09-26 20:10:34 -0300

    Fixing failed tests

--- __tests__/unit/movie.test.js
+++ __tests__/unit/movie.test.js
@@ -1,9 +1,7 @@
-import Movie from '../../src/app/models/Movie';
+import movie from '../../src/app/models/Movie';
 
 describe('Tests of model', () => {
   it('should return model', () => {
-    const movie = new Movie();
-
     expect(movie).not.toBeNull();
   });
 });
